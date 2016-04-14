7751bfdb4bcf9ca96c32dca646b0d6b8e8261195
Matthias Richard <matthias.rms@gmail.com>
2016-04-14 22:11:46 +0200

    logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   get '/webhook', to: 'analyzer#webhook'
   post '/webhook', to: 'analyzer#webhook_post'
+
+  get '/new_message', to: 'conversation#new_message'
+  get '/new_conversation', to: 'conversation#new'
 end
