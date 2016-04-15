afe4bbdfffc67058015448c22e56e6a18c8fe0f6
Matthias Richard <matthias.rms@gmail.com>
2016-04-15 16:57:10 +0200

    fixed routes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,7 +10,7 @@ class ConversationsController < ApplicationController
   def request(text)
     token = "CAAKs4sjMLtgBACbNSA3adhDT76dxu4A2iqNsZBcsfPgCMeVBZCbB7yGI5SiPU6PbfpFyi2W7zEclj8YXYxCG9VLcWZCBVT4XsBBEFJt6tAH8XYu1Y0W6BJsT2L6YNSvHnYV6pAgIaZB7HWrzchURHT0eSdyFB8OKR0wkkhjg0yatEx3XBIZAedcSRZAFXuSHIZD"
     url = "https://graph.facebook.com/v2.6/me/messages?"
-
+    puts params
      request_params =  {
         recipient: {id: sender},
         message: {text: text},
@@ -32,7 +32,7 @@ class ConversationsController < ApplicationController
 
   def answer
     text = "hell"
-    request("hello")
+    request(text: "hello")
   end
 
   def new_message
