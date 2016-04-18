902bbddaacfb3a6ad57f1a1cbe32ef9156360695
Matthias Richard <matthias.rms@gmail.com>
2016-04-18 20:25:46 +0200

    fixed

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -12,12 +12,8 @@ token = '185608577:AAGrm70H1VlXnd3A8mEE6KbGWlqUNPGFPPc'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when 'start'
       bot.api.send_message(chat_id: message.chat.id, text: "I am the SitePoint bot, My commands are /sitepoint /map")
-    when 'sitepoint'
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome to http://sitepoint.com")
-    when 'map'
       bot.api.send_location(chat_id: message.chat.id, latitude: -37.807416, longitude: 144.985339)
    end
   end
