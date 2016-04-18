2bca1f73fde92c190eb9503396bdb7328504674b
Matthias Richard <matthias.rms@gmail.com>
2016-04-18 18:49:35 +0200

    fixed params

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,9 +8,10 @@ class TwilioController < ApplicationController
   def text
       account_sid = "AC27fc8675a5340aea2d9c1fdd6756160f"
       auth_token = "bb79881b9d4c0d2b31126f13259026be"
+      puts params["from"]
       @client = Twilio::REST::Client.new account_sid, auth_token
       @message = @client.messages.create(
-        to: params["from"],
+        to: "+33632621718",
         from: "+33756797305",
         body: "Hello!"
       )
