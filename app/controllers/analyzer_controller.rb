7baeb5ab60af25d2cc1c6ea0c8d9eb63eaa20e5f
Matthias Richard <matthias.rms@gmail.com>
2016-04-23 10:35:23 +0200

    hh

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -114,11 +114,11 @@ end
       },
       :run_query => -> (session_id, context) {
         @session = Session.find(session_id)
-fb_request(1006889982732663, "hey")
+
     @products = Oj.load(RestClient.get 'https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/products.json?collection_id=263046279')
     @products["products"].each do |h1|
+      fb_request(1006889982732663, "hey")
       p h1["title"]
-
     end
   #       request_params =  {
   #   recipient: {id: 1006889982732663},
