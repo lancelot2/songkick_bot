a2b3f4f93b98f5b99b94c8a1ec2ddff5716347cd
Matthias Richard <matthias.rms@gmail.com>
2016-04-23 11:21:55 +0200

    fix

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -107,18 +107,13 @@ end
         @session.context = context
         @session.save
         p context
+        p "hey"
         return context
       },
       :error => -> (session_id, context, error) {
         p 'Oops I don\'t know what to do.'
       },
       :run_query => -> (session_id, context) {
-        @session = Session.find(session_id)
-        p "hello"
-    # @products = Oj.load(RestClient.get 'https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/products.json?collection_id=263046279')
-    # @products["products"].each do |h1|
-    #   p h1["title"]
-    # end
 
         return context
       }
