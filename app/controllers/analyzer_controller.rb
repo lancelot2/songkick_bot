897a7fe9050f6884b4414e6c74f081de28fdbd73
Matthias Richard <matthias.rms@gmail.com>
2016-04-23 12:38:23 +0200

    fix

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -113,7 +113,7 @@ end
       },
       :run_query => -> (session_id, context) {
         @session = Session.find(session_id)
-        @products = Oj.load(RestClient.get "https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/products.json?collection_id=263046279&product_type#{context['style']}")
+        @products = Oj.load(RestClient.get "https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/products.json?collection_id=263046279&product_type=#{context['style']}")
         request_params =  {
             recipient: {id: @session.facebook_id},
             message: {
--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -3,7 +3,7 @@ class ShopifyController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def req
-   @products = Oj.load(RestClient.get 'https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/products.json?collection_id=263046279&product_type=lifestyle')
+   @products = Oj.load(RestClient.get 'https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/products.json?collection_id=263046279&product_type=running')
    @collection = RestClient.get 'https://91b97aeb761861c20b777ede328d512e:ec169cbd05bcd7db7b03f5d6291a3f58@myshopifybot.myshopify.com/admin/custom_collections/263046279.json'
   end
 
