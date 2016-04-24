9c9fa9b1b9624b114567eb0d647c8181b7f50429
Matthias Richard <matthias.rms@gmail.com>
2016-04-24 20:44:03 +0200

    push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+ window.fbAsyncInit = function() {
+  console.log("he");
+    FB.init({
+      appId      : '753039984832216',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -214,6 +214,8 @@ end
     }
 
     client = Wit.new access_token, @actions
+    p "PARAMS:"
+    p params
     if params["entry"][0]["messaging"][0]["delivery"].nil? && params["entry"][0]["messaging"][0]["postback"].nil?
         msg = params["entry"][0]["messaging"][0]["message"]["text"]
         sender = params["entry"][0]["messaging"][0]["sender"]["id"]
