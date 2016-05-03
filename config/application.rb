ec7957fb4925c1f5980d7a780c01880352531ce4
Matthias Richard <matthias.rms@gmail.com>
2016-05-03 21:30:57 +0200

    fixed

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module RailsBot
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += %W(#{config.root}/app/bot)
-    config.autoload_paths += Dir["#{config.root}/app/bot/**/"]
+    config.autoload_paths << Rails.root.join('app/bot/**/')
   end
 end
